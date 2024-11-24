57ced509193f2845792fee7d13cd5dcd329cd147
Marcelo Costa da Mata <mcosta.matta@gmail.com>
2024-11-24 11:36:02 -0300

    Criação do menu de opções, do/ehile e switch case

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Criação do Repositório;
 Criação do projeto;
 Desenvolvimento da classe Calculos, seu construtor, getter, setter e métodos soma, subtração, multiplicação, divisão e radiação;
+Criação do menu de opções. do/while e switch case;
--- src/main/java/appcalculadora/AppCalculadora.java
+++ src/main/java/appcalculadora/AppCalculadora.java
@@ -1,6 +1,47 @@
 package appcalculadora;
 
+import java.util.Scanner;
+
 public class AppCalculadora {
+    public static void mostrarMenu() {
+        System.out.println("-----------Seja bem vindo-----------");
+        System.out.println("-----Selecione a opção desejada-----");
+        System.out.println("|----1 - Soma----------------------|");
+        System.out.println("|----2 - Subtração-----------------|");
+        System.out.println("|----3 - Multiplicação-------------|");
+        System.out.println("|----4 - Divisão-------------------|");
+        System.out.println("|----5 - Raiz Quadrada-------------|");
+        System.out.println("|----0 - Sair----------------------|");
+    }
+
+    public static void menuOpcoes() {
+        try(Scanner input = new Scanner(System.in)) {
+            int opcao = -1;
+
+            do {
+                mostrarMenu();
+                System.out.println("Escolha uma opção: ");
+
+                if (input.hasNextInt()) {
+                    opcao = input.nextInt();
+                    input.nextLine();
+                } else {
+                    System.out.println("Entrada inválida! Digite um número");
+                    continue;
+                }
+
+                switch (opcao) {
+                    case 1:
+                        
+                        break;
+                
+                    default:
+                        break;
+                }
+            } while (opcao == 0);
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
