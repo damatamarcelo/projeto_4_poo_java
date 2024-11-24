36490a00c816878ed25ef7784d057133e062d03c
Marcelo Costa da Mata <mcosta.matta@gmail.com>
2024-11-23 21:29:13 -0300

    Desenvolvimento da classe Calculos

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Criação do Repositório;
 Criação do projeto;
+Desenvolvimento da classe Calculos, seu construtor, getter, setter e métodos soma, subtração, multiplicação, divisão e radiação;
--- src/main/java/appcalculadora/Calculos.java
+++ src/main/java/appcalculadora/Calculos.java
@@ -1,5 +1,42 @@
 package appcalculadora;
 
 public class Calculos {
+    private double x;
 
+    public Calculos(double x) {
+        this.x = x;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double soma(double x, double y) {
+        setX(x + y);
+        return getX();
+    }
+
+    public double subtracao(double x, double y) {
+        setX(x - y);
+        return getX();
+    }
+
+    public double multiplicacao(double x, double y) {
+        setX(x * y);
+        return getX();
+    }
+
+    public double divisao(double x, double y) {
+        setX(x / y);
+        return getX();
+    }
+
+    public double radiciacao(double x) {
+        setX(Math.sqrt(x));
+        return getX();
+    }
 }
